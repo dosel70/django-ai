6bc6d80cbb3eec2b8dfb9ed6701558bd29ed5e75
jo <doselaka7059@gmail.com>
2024-05-22 20:12:21 +0900

    gd

--- oneLabProject/settings.py
+++ oneLabProject/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m&1gjc)a!0m&t4-skkigdplzw%e&f)5j@ksztwce%#bqaksqs)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '52.79.255.249', 'onelab.life']
 
 
 # Application definition
